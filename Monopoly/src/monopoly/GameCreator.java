6289d176597bc10a53b179dfe30f3d43a5d9b5ca
SeanSimpkins <Sean.Simpkinsnz@gmail.com>
2021-09-01 14:55:21 +1200

    Housing names
    update

--- Monopoly/src/monopoly/GameCreator.java
+++ Monopoly/src/monopoly/GameCreator.java
@@ -39,7 +39,33 @@ public class GameCreator {
         }
     }
 
-    public void createLocations() {
+    public void createLocations(){
+    this.locations[0] = new GoLocation("Go", 0);
+    this.locations[1] = new HousingLocation("Bay of Islands", 1, 10000, 0, 5000, 5000);
+    this.locations[2] = new HousingLocation("Milford Sound", 2, 10000, 0, 5000, 5000);
+    this.locations[3] = new ChanceLocation("Chance", 2, 2, "this is a chance message");
+    this.locations[4] = new HousingLocation("Queenstown", 4, 10000, 0, 5000, 5000);
+    this.locations[5] = new HousingLocation("Lake Taupo", 5, 10000, 0, 5000, 5000);
+    this.locations[6] = new GotoJail("Go to jail", 6);
+    this.locations[7] = new HousingLocation("Rotorua", 7, 10000, 0, 5000, 5000);
+    this.locations[8] = new HousingLocation("Fox Glaciers", 8, 10000, 0, 5000, 5000);
+    this.locations[9] = new ChanceLocation("Chance", 2, 2, "this is a chance message");
+    this.locations[10] = new HousingLocation("Mount Cook", 10, 10000, 0, 5000, 5000);
+    this.locations[11] = new HousingLocation("Hawke's Bay", 11, 10000, 0, 5000, 5000);
+    this.locations[12] = new GotoJail("Go to jail", 12);
+    this.locations[13] = new HousingLocation("Auckland", 13, 10000, 0, 5000, 5000);
+    this.locations[14] = new HousingLocation("Coromandel Peninsula", 14, 10000, 0, 5000, 5000);
+    this.locations[15] = new ChanceLocation("Chance", 2, 2, "this is a chance message");
+    this.locations[16] = new HousingLocation("Kaikoura", 16, 10000, 0, 5000, 5000);
+    this.locations[17] = new HousingLocation("Fiordland", 17, 10000, 0, 5000, 5000);
+    this.locations[18] = new ChanceLocation("Chance", 2, 2, "this is a chance message");
+    this.locations[19] = new HousingLocation("Chatham Island", 19, 10000, 0, 5000, 5000);
+    this.locations[20] = new HousingLocation("Wellington", 20, 10000, 0, 5000, 5000);
+    this.locations[21] = new ChanceLocation("Chance", 2, 2, "this is a chance message");
+    this.locations[22] = new HousingLocation("Christchurch", 22, 10000, 0, 5000, 5000);
+    this.locations[23] = new HousingLocation("Stewwart Island", 23, 10000, 0, 5000, 5000);    
+    }
+    /*{
         for (int i = 0; i < 24; i++) {
             
             if (i == 0) {
@@ -63,7 +89,7 @@ public class GameCreator {
             }
         }
     }
-
+*/
     public GlobalLocation[] getLocations() {
         return locations;
     }
