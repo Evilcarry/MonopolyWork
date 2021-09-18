268e1271f06854d40241b24157cb4aef5260ad08
Evilcarry <benjhithaxx@gmail.com>
2021-09-18 18:32:16 +1200

    Created a database manager
    Created a database manager, still need to implement a way to work with it, going to integrate gamesavingandloading to it

---
+++ Monopoly/src/monopoly/MonopolyDBManager.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package monopoly;
+
+/**
+ *
+ * @author benjh
+ */
+public class MonopolyDBManager {
+    // maybe the users can have different games so if they played a 2 player game they can save it to their own save.
+    //Have a look at the H01_DBanager.java file for more info.
+    //Have a look at H_RetrieveAll.
+}
